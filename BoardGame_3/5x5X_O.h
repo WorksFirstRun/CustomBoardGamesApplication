f93a9cbd713d47b1b4f6db50210a184b13063d32
Mindlessdev <123970239+Begginer-dev@users.noreply.github.com>
2024-12-05 17:20:00 +0200

    Update 5x5X_O.h

--- BoardGame_3/5x5X_O.h
+++ BoardGame_3/5x5X_O.h
@@ -8,13 +8,12 @@
 template <typename T> class FiveByFive_Board;
 template <typename T> class FiveByFive_HumanPlayer;
 template <typename T> class FiveByFive_RandomPlayer;
-template <typename T> class 5x5_MinMaxPlayer;
 
 //================= Board Class =================
 template <typename T>
 class FiveByFive_Board : public Board<T> {
 private:
-    int countThreeInARow(T symbol); 
+    int countThreeInARow(T symbol);
     bool isValidMove(int x, int y, T symbol);
 
 public:
@@ -75,12 +74,24 @@ public:
         }
         return false;
     }
+    string whoWon(const string& player1Name, const string& player2Name) {
+        int count_X = countThreeInARow('X');
+        int count_O = countThreeInARow('O');
 
+        if (this->n_moves == 24) {  
+            if (count_X > count_O) {
+                return player1Name;  
+            } else if (count_O > count_X) {
+                return player2Name;  
+            }
+        }
+        return ""; 
+    }
     bool game_is_over() override {
-        return this->n_moves == 24;  
+        return this->n_moves == 24;
     }
 
-    
+
     T** GetBoardState() {
         return this->board;
     }
@@ -93,74 +104,19 @@ public:
 //================= Human Player Class =================
 template <typename T>
 class FiveByFive_HumanPlayer : public Player<T> {
-private:
-    static int player_count;  
-    int player_number;        
-
 public:
-    FiveByFive_HumanPlayer() : Player<T>("", ' ') {
-        player_count++;
-        if (player_count > 2) {
-            throw runtime_error("Cannot create more than 2 players");
-        }
-        
-        player_number = player_count;
-        initializePlayer();
-    }
 
-    void initializePlayer() {
-        string name;
-        T symbol;
-        
-        cout << "\nPlayer " << player_number << " setup:" << endl;
-        cout << "Enter your name: ";
-        cin >> name;
-        
-        do {
-            cout << "Choose your symbol (X or O): ";
-            cin >> symbol;
-            symbol = toupper(symbol);  
-        } 
-        
-        while (symbol != 'X' && symbol != 'O');
-
-        // If player 2 chooses the same symbol as player 1  give them the other symbol
-        if (player_number == 2 && symbol == this->other_symbol) {
-            cout << "Symbol already taken. You will be assigned: " 
-
-                 << (symbol == 'X' ? 'O' : 'X') << endl;
-            symbol = (symbol == 'X' ? 'O' : 'X');
-        }
-        
-        this->name = name;
-        this->symbol = symbol;
-        
-        if (player_number == 1) {
-            this->other_symbol = symbol;  
-        }
-    }
+    FiveByFive_HumanPlayer(string n,T symbol) : Player<T>(n,symbol){}
 
     void getmove(int& x, int& y) override {
         cout << "\nPlayer " << this->name << "'s turn (" << this->symbol << ")" << endl;
         cout << "Please enter your move x and y (0 to 4) separated by spaces: ";
-        
-            cin >> x >> y;
-            
-    }
 
-    static void resetPlayerCount() {
-        player_count = 0;
-    }
+        cin >> x >> y;
 
-private:
-    static T other_symbol; 
+    }
 };
 
-template <typename T>
-int FiveByFive_HumanPlayer<T>::player_count = 0;
-
-template <typename T>
-T FiveByFive_HumanPlayer<T>::other_symbol = ' ';
 
 //================= Random Player Class =================
 template <typename T>
@@ -181,7 +137,7 @@ public:
             x = rand() % dimension;
             y = rand() % dimension;
         }
-        this->boardPtr->update_board(x, y, '.');  
+        this->boardPtr->update_board(x, y, '.');
     }
 };
 
@@ -191,51 +147,51 @@ public:
 template <typename T>
 int FiveByFive_Board<T>::countThreeInARow(T symbol) {
     int count = 0;
-    
+
     // Check horizontal three-in-a-row
     for (int i = 0; i < this->rows; i++) {
         for (int j = 1; j < this->columns - 1; j++) {
-            if (this->board[i][j] == symbol && 
-                this->board[i][j-1] == symbol && 
+            if (this->board[i][j] == symbol &&
+                this->board[i][j-1] == symbol &&
                 this->board[i][j+1] == symbol) {
                 count++;
             }
         }
     }
-    
+
     // Check vertical three-in-a-row
     for (int i = 1; i < this->rows - 1; i++) {
         for (int j = 0; j < this->columns; j++) {
-            if (this->board[i][j] == symbol && 
-                this->board[i-1][j] == symbol && 
+            if (this->board[i][j] == symbol &&
+                this->board[i-1][j] == symbol &&
                 this->board[i+1][j] == symbol) {
                 count++;
             }
         }
     }
-    
+
     // Check diagonal (top-left to bottom-right)
     for (int i = 1; i < this->rows - 1; i++) {
         for (int j = 1; j < this->columns - 1; j++) {
-            if (this->board[i][j] == symbol && 
-                this->board[i-1][j-1] == symbol && 
+            if (this->board[i][j] == symbol &&
+                this->board[i-1][j-1] == symbol &&
                 this->board[i+1][j+1] == symbol) {
                 count++;
             }
         }
     }
-    
+
     // Check diagonal (top-right to bottom-left)
     for (int i = 1; i < this->rows - 1; i++) {
         for (int j = 1; j < this->columns - 1; j++) {
-            if (this->board[i][j] == symbol && 
-                this->board[i-1][j+1] == symbol && 
+            if (this->board[i][j] == symbol &&
+                this->board[i-1][j+1] == symbol &&
                 this->board[i+1][j-1] == symbol) {
                 count++;
             }
         }
     }
-    
+
     return count;
 }
 
@@ -244,12 +200,83 @@ bool FiveByFive_Board<T>::isValidMove(int x, int y, T symbol) {
     if (x < 0 || x >= this->rows || y < 0 || y >= this->columns) {
         return false;
     }
-    
+
     if (symbol != '.') {
         return this->board[x][y] == '.';
     }
-    
+
     return this->board[x][y] != '.';
 }
 
-#endif 
+
+
+void RunBoardGame(){
+    int choice;
+
+    Board<char> * board = new FiveByFive_Board<char>();
+    Player<char> * players[2];
+
+    string player1,player2;
+
+
+    cout << "Five by Five XO GameBoard3 \n";
+
+    // Set up player 1
+    cout << "Enter Player 1 name (symbol is X): ";
+    cin >> player1;
+    cout << "Choose Player X type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cout << "3. Smart Computer (AI)\n";
+    cin >> choice;
+
+    switch(choice){
+        case 1:
+            players[0] = new FiveByFive_HumanPlayer<char>(player1,'X');
+            break;
+        case 2:
+            players[0] = new FiveByFive_RandomPlayer<char>('X');
+            players[0]->setBoard(board);
+            break;
+        case 3:
+
+        default:
+            break;
+    }
+
+
+    cout << "Enter Player 2 name (symbol is O): ";
+    cin >> player2;
+    cout << "Choose Player O type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cout << "3. Smart Computer (AI)\n";
+    cin >> choice;
+
+    switch(choice){
+        case 1:
+            players[1] = new FiveByFive_HumanPlayer<char>(player2,'O');
+            break;
+        case 2:
+            players[1] = new FiveByFive_RandomPlayer<char>('O');
+            players[1]->setBoard(board);
+            break;
+        case 3:
+
+            break;
+        default:
+            break;
+    }
+
+    GameManager<char> fourInRowGameManager(board,players);
+
+    fourInRowGameManager.run();
+
+    delete board;
+
+    for (auto & player : players){
+        delete player;
+    }
+}
+
+#endif
